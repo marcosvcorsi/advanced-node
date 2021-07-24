f425aa5f4a5d2d76a20745b5bdedd0587bcf7d97
Marcos Corsi <marcosvcorsi@gmail.com>
2021-07-24 20:22:40 -0300

    test: ensure access token expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,5 +1,11 @@
 export class AccessToken {
+  static EXPIRATION_IN_MINUTES = 30;
+
   constructor(
     private readonly value: string,
   ) {}
+
+  static get expirationInMs(): number {
+    return AccessToken.EXPIRATION_IN_MINUTES * 60 * 1000;
+  }
 }
