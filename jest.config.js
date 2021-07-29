62798f9da7d308412de0fffdbbbc0002b1fe0f0d
Marcos Corsi <marcosvcorsi@gmail.com>
2021-07-28 22:08:48 -0300

    chore: fix files test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**/*.ts',
+    '!<rootDir>/src/**/index.ts',
+  ],
   coverageDirectory: "coverage",
   moduleNameMapper: {
     '@/tests/(.+)$': '<rootDir>/tests/$1',
