595e29cf0be7dabaced152bee9bfa6da66e33135
Marcos Corsi <marcosvcorsi@gmail.com>
2021-07-30 20:27:01 -0300

    fix: add axios http client use http get client

--- src/infra/http/axios-client.ts
+++ src/infra/http/axios-client.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { HttpGetClient } from '@/infra/http';
 
-export class AxiosHttpClient {
+export class AxiosHttpClient implements HttpGetClient {
   async get<T = any>({ url, params } : HttpGetClient.Params): Promise<T> {
     const { data } = await axios.get<T>(url, { params });
 
