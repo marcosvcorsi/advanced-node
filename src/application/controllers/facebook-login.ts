827975556eaa3e48a39adefe6bc12ffe4329fecb
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-04 21:04:00 -0300

    refactor: apply server error helper

--- src/application/controllers/facebook-login.ts
+++ src/application/controllers/facebook-login.ts
@@ -1,5 +1,7 @@
-import { RequiredFieldError, ServerError } from '@/application/errors';
-import { badRequest, HttpResponse, unauthorized } from '@/application/helpers';
+import { RequiredFieldError } from '@/application/errors';
+import {
+  badRequest, HttpResponse, serverError, unauthorized,
+} from '@/application/helpers';
 import { AuthenticationError } from '@/domain/errors';
 import { FacebookAuthentication } from '@/domain/features';
 
@@ -29,10 +31,7 @@ export class FacebookLoginController {
         },
       };
     } catch (error) {
-      return {
-        statusCode: 500,
-        data: new ServerError(error),
-      };
+      return serverError(error);
     }
   }
 }
