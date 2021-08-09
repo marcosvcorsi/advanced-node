0e86e14a8ec279a04286db69bea0033a8c32c80d
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-08 22:37:39 -0300

    fix: move token integration to env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,6 +2,7 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID,
     clientSecret: process.env.FB_CLIENT_SECRET,
+    tokenIntegration: process.env.FB_TOKEN_INTEGRATION,
   },
   port: process.env.PORT ?? 3000,
 };
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -22,7 +22,7 @@ describe('FacebookApi Integration Test', () => {
 
   it('should return a Facebook User if token is valid', async () => {
     const fbUser = await sut.loadUser({
-      token: 'EAANRAxmmNaIBAB7LwqZBL9xez5FtMEZCnKJDClZAENdFZBI9xQ1q2EK1ZAju7Re9V45TKy64MOAWeIC7vSSaKxUF8qJmJeY0MouGsIEZAlcAFGDHB5JPzKPGN77BhqucXwu5Kq3XMOk6VXqPX5w7GEApCaiZA8jLtf1DZB7IPcRjzbvjZCnGyoDljCx1mLMtNBOmATjKkikQvvBbovtbBFZA62',
+      token: String(env.facebookApi.tokenIntegration),
     });
 
     expect(fbUser).toEqual({
