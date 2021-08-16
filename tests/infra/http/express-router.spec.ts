8eb7b745a724635cf91733c94ded41c323a04d08
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-15 22:20:12 -0300

    feat: ensure ExpressRouter return correct error values

--- tests/infra/http/express-router.spec.ts
+++ tests/infra/http/express-router.spec.ts
@@ -14,6 +14,10 @@ class ExpressRouter {
 
     const { statusCode, data } = await this.controller.handle({ ...body });
 
+    if (statusCode >= 400) {
+      return response.status(statusCode).json({ error: data.message });
+    }
+
     return response.status(statusCode).json(data);
   }
 }
@@ -68,6 +72,38 @@ describe('ExpressRouter', () => {
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.status).toHaveBeenCalledTimes(1);
     expect(res.json).toHaveBeenCalledWith({ any: 'data' });
+    expect(res.json).toHaveBeenCalledTimes(1);
+  });
+
+  it('should response with 400 and valid error', async () => {
+    const error = new Error('any_error');
+
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 400,
+      data: error,
+    });
+
+    await sut.adapt(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ error: error.message });
+    expect(res.json).toHaveBeenCalledTimes(1);
+  });
+
+  it('should response with 500 and valid error', async () => {
+    const error = new Error('any_error');
+
+    controller.handle.mockResolvedValueOnce({
+      statusCode: 500,
+      data: error,
+    });
+
+    await sut.adapt(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.status).toHaveBeenCalledTimes(1);
+    expect(res.json).toHaveBeenCalledWith({ error: error.message });
     expect(res.json).toHaveBeenCalledTimes(1);
   });
 });
