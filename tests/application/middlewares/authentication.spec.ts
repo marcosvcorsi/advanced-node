4d4a55853c0fbd38c60cb09b1f814516955a67be
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-29 02:22:13 -0300

    feat: ensure AuthenticationMiddleware calls authorize

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,23 +1,48 @@
 import { UnauthorizedError } from '@/application/errors';
 import { HttpResponse, unauthorized } from '@/application/helpers';
+import { RequiredStringValidator } from '@/application/validation';
+import { Authorize } from '@/domain/use-cases';
 
 type HttpRequest = {
   authorization: string
 }
 
 class AuthenticationMiddleware {
-  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    console.log(httpRequest);
+  constructor(
+    private readonly authorize: Authorize,
+  ) {}
 
-    return unauthorized();
+  async handle({ authorization }: HttpRequest): Promise<HttpResponse | undefined> {
+    const error = new RequiredStringValidator(authorization, 'authorization').validate();
+
+    if (error) {
+      return unauthorized();
+    }
+
+    await this.authorize({
+      token: authorization,
+    });
+
+    return undefined;
   }
 }
 
 describe('AuthenticationMiddleware', () => {
+  let authorization: string;
+  let authorize: jest.Mock;
+
   let sut: AuthenticationMiddleware;
 
+  beforeAll(() => {
+    authorization = 'any_token';
+
+    authorize = jest.fn();
+  });
+
   beforeEach(() => {
-    sut = new AuthenticationMiddleware();
+    sut = new AuthenticationMiddleware(
+      authorize,
+    );
   });
 
   it('should return 401 if authorization is empty', async () => {
@@ -45,5 +70,12 @@ describe('AuthenticationMiddleware', () => {
       statusCode: 401,
       data: new UnauthorizedError(),
     });
+  });
+
+  it('should call Authorized with correct params', async () => {
+    await sut.handle({ authorization });
+
+    expect(authorize).toHaveBeenCalledWith({ token: authorization });
+    expect(authorize).toHaveBeenCalledTimes(1);
   });
 });
