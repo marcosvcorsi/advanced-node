31f06da30ee5a1836364aaffddb85ac11e28a22d
Marcos Corsi <marcosvcorsi@gmail.com>
2021-08-29 20:24:49 -0300

    chore: change database and environment variables

--- src/main/server.ts
+++ src/main/server.ts
@@ -6,9 +6,8 @@ import 'dotenv/config';
 import { createConnection } from 'typeorm';
 
 import { app } from '@/main/app';
-import { connectionOptions } from '@/main/config/database';
 import { env } from '@/main/config/env';
 
-createConnection(connectionOptions)
+createConnection()
   .then(() => app.listen(env.port, () => console.log(`Server is running at ${env.port}`)))
   .catch(console.error);
