1bb1c3bb20f02990088791b1a29c27e6884bdf7e
Marcos Corsi <marcosvcorsi@gmail.com>
2021-09-02 21:31:50 -0300

    feat: ensure auth middleware returns 200 with locals

--- tests/main/middlewares/authentication.spec.ts
+++ tests/main/middlewares/authentication.spec.ts
@@ -1,12 +1,18 @@
+import { sign } from 'jsonwebtoken';
 import request from 'supertest';
 
 import { UnauthorizedError } from '@/application/errors';
 import { app } from '@/main/app';
+import { env } from '@/main/config/env';
 import { auth } from '@/main/middlewares/authentication';
 
 describe('AuthenticationMiddleware', () => {
+  let userId: string;
+
   beforeAll(() => {
-    app.get('/fake_route', auth, (req, res) => res.send('OK'));
+    userId = 'any_user_id';
+
+    app.get('/fake_route', auth, (req, res) => res.json(req.locals));
   });
 
   it('should return 401 if authorization header was not provided', async () => {
@@ -15,5 +21,16 @@ describe('AuthenticationMiddleware', () => {
 
     expect(response.status).toBe(401);
     expect(response.body.error).toBe(new UnauthorizedError().message);
+  });
+
+  it('should return 200 if authorization header was provided', async () => {
+    const token = sign({ key: userId }, env.jwtSecret);
+
+    const response = await request(app)
+      .get('/fake_route')
+      .set('Authorization', token);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual({ userId });
   });
 });
