855e0ed42084fe3b89299b7aacf38d8acb2e733c
Marcos Corsi <marcosvcorsi@gmail.com>
2021-10-16 21:43:17 -0300

    feat: ensure SaveProfilePictureController return 400 if file is not provided

---
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -0,0 +1,42 @@
+import { Controller } from '@/application/controllers';
+import { RequiredFieldError } from '@/application/errors';
+import { badRequest, HttpResponse } from '@/application/helpers';
+
+type HttpRequest = {
+  file: unknown;
+}
+
+type Response = Error;
+
+class SaveProfilePictureController extends Controller {
+  async perform(httpRequest: HttpRequest): Promise<HttpResponse<Response>> {
+    console.log(httpRequest);
+    return badRequest(new RequiredFieldError('file'));
+  }
+}
+
+describe('SaveProfilePictureController', () => {
+  let sut: SaveProfilePictureController;
+
+  beforeEach(() => {
+    sut = new SaveProfilePictureController();
+  });
+
+  it('should return 400 if file is not provided', async () => {
+    const response = await sut.handle({ file: undefined });
+
+    expect(response).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file'),
+    });
+  });
+
+  it('should return 400 if file is not provided', async () => {
+    const response = await sut.handle({ file: null });
+
+    expect(response).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file'),
+    });
+  });
+});
