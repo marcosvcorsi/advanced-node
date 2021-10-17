fd997ec7da91e34e9369e7d032e939bb372f674b
Marcos Corsi <marcosvcorsi@gmail.com>
2021-10-16 21:57:06 -0300

    feat: ensure SaveProfilePictureController return 400 if file mimetype is invalid

--- tests/application/controllers/save-profile-picture.spec.ts
+++ tests/application/controllers/save-profile-picture.spec.ts
@@ -2,21 +2,46 @@ import { Controller } from '@/application/controllers';
 import { RequiredFieldError } from '@/application/errors';
 import { badRequest, HttpResponse } from '@/application/helpers';
 
+class InvalidMimeTypeError extends Error {
+  constructor(allowed: string[]) {
+    super(`Unsupported mime type. allowed: ${allowed.join(',')}`);
+
+    this.name = 'InvalidMimeTypeError';
+  }
+}
+
 type HttpRequest = {
-  file: unknown;
+  file: {
+    buffer: Buffer;
+    mimetype: string;
+  };
 }
 
 type Response = Error;
 
 class SaveProfilePictureController extends Controller {
-  async perform(httpRequest: HttpRequest): Promise<HttpResponse<Response>> {
-    console.log(httpRequest);
-    return badRequest(new RequiredFieldError('file'));
+  async perform({ file }: HttpRequest): Promise<HttpResponse<Response>> {
+    if (!file?.buffer?.length) {
+      return badRequest(new RequiredFieldError('file'));
+    }
+
+    return badRequest(new InvalidMimeTypeError(['jpg', 'jpeg', 'png']));
   }
 }
 
 describe('SaveProfilePictureController', () => {
+  let buffer: Buffer;
+  // let mimeType: string;
+  // let file: { buffer: Buffer };
+
   let sut: SaveProfilePictureController;
+
+  beforeAll(() => {
+    buffer = Buffer.from('any_file');
+    // mimeType = 'image/png';
+
+    // file = { buffer };
+  });
 
   beforeEach(() => {
     sut = new SaveProfilePictureController();
@@ -46,6 +71,15 @@ describe('SaveProfilePictureController', () => {
     expect(response).toEqual({
       statusCode: 400,
       data: new RequiredFieldError('file'),
+    });
+  });
+
+  it('should return 400 if file type is invalid', async () => {
+    const response = await sut.handle({ file: { buffer, mimeType: 'invalid_type' } });
+
+    expect(response).toEqual({
+      statusCode: 400,
+      data: new InvalidMimeTypeError(['jpg', 'jpeg', 'png']),
     });
   });
 });
