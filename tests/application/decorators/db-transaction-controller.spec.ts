f654f8e166ffdb76a5274f76fde2fa25df36d5a6
Marcos Corsi <marcosvcorsi@gmail.com>
2021-12-07 14:04:58 -0300

    feat: ensure DbTransaction controller calls rollback and close transaction

--- tests/application/decorators/db-transaction-controller.spec.ts
+++ tests/application/decorators/db-transaction-controller.spec.ts
@@ -6,6 +6,7 @@ interface DbTransaction {
   openTransaction: () => Promise<void>;
   closeTransaction: () => Promise<void>;
   commit: () => Promise<void>;
+  rollback: () => Promise<void>;
 }
 
 class DbTransactionController {
@@ -17,10 +18,15 @@ class DbTransactionController {
   async perform(httpRequest: any): Promise<void> {
     await this.db.openTransaction();
 
-    await this.decorate.perform(httpRequest);
+    try {
+      await this.decorate.perform(httpRequest);
 
-    await this.db.commit();
-    await this.db.closeTransaction();
+      await this.db.commit();
+    } catch {
+      await this.db.rollback();
+    } finally {
+      await this.db.closeTransaction();
+    }
   }
 }
 
@@ -60,8 +66,21 @@ describe('DbTransactionController', () => {
   it('should call commit and close transaction on success', async () => {
     await sut.perform(params);
 
+    expect(db.rollback).not.toHaveBeenCalled();
     expect(db.commit).toHaveBeenCalled();
     expect(db.commit).toHaveBeenCalledTimes(1);
+    expect(db.closeTransaction).toHaveBeenCalled();
+    expect(db.closeTransaction).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call rollback and close transaction on failure', async () => {
+    decorate.perform.mockRejectedValueOnce(new Error('any_error'));
+
+    await sut.perform(params);
+
+    expect(db.commit).not.toHaveBeenCalled();
+    expect(db.rollback).toHaveBeenCalled();
+    expect(db.rollback).toHaveBeenCalledTimes(1);
     expect(db.closeTransaction).toHaveBeenCalled();
     expect(db.closeTransaction).toHaveBeenCalledTimes(1);
   });
